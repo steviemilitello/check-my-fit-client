710374b207b72a419c903cff2b736cb6c1096dd7
kaiguy920 <smithkai920@gmail.com>
2022-04-25 16:36:51 -0400

    adds view ouf outfits for each tag search

--- src/api/tag.js
+++ src/api/tag.js
@@ -1,7 +1,75 @@
 import apiUrl from '../apiConfig'
 import axios from 'axios'
 
-// index user outfit function for a profile view
+// index outfits based on tag
+export const getVintageOutfits = () => {
+    return axios(`${apiUrl}/tags/vintage`)
+}
+
 export const getCasualOutfits = () => {
     return axios(`${apiUrl}/tags/casual`)
+}
+
+export const getGrungeOutfits = () => {
+    return axios(`${apiUrl}/tags/grunge`)
+}
+
+export const getGothOutfits = () => {
+    return axios(`${apiUrl}/tags/goth`)
+}
+
+export const getPunkOutfits = () => {
+    return axios(`${apiUrl}/tags/punk`)
+}
+
+export const getBohoOutfits = () => {
+    return axios(`${apiUrl}/tags/boho`)
+}
+
+export const getArtsyOutfits = () => {
+    return axios(`${apiUrl}/tags/artsy`)
+}
+
+export const getDateNightOutfits = () => {
+    return axios(`${apiUrl}/tags/datenight`)
+}
+
+export const getSummerLookOutfits = () => {
+    return axios(`${apiUrl}/tags/summerlook`)
+}
+
+export const getSpringLookOutfits = () => {
+    return axios(`${apiUrl}/tags/springlook`)
+}
+
+export const getFallLookOutfits = () => {
+    return axios(`${apiUrl}/tags/falllook`)
+}
+
+export const getWinterLookOutfits = () => {
+    return axios(`${apiUrl}/tags/winterlook`)
+}
+
+export const getWesternOutfits = () => {
+    return axios(`${apiUrl}/tags/western`)
+}
+
+export const getSexyOutfits = () => {
+    return axios(`${apiUrl}/tags/sexy`)
+}
+
+export const getSportyOutfits = () => {
+    return axios(`${apiUrl}/tags/sporty`)
+}
+
+export const getFormalOutfits = () => {
+    return axios(`${apiUrl}/tags/formal`)
+}
+
+export const getDayOutfits = () => {
+    return axios(`${apiUrl}/tags/day`)
+}
+
+export const getEveningOutfits = () => {
+    return axios(`${apiUrl}/tags/evening`)
 }
---
+++ src/components/tag/SummerLook.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import { getSummerLookOutfits } from '../../api/tag'
+import { Card, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { indexOutfitsSuccess, indexOutfitsFailure } from '../shared/AutoDismissAlert/messages'
+
+const cardContainerLayout = {
+    display: 'flex',
+    justifyContent: 'center',
+    flexFlow: 'row wrap'
+}
+
+const IndexSummerLookOutfits = (props) => {
+
+    const [outfits, setOutfits] = useState(null)
+    const { msgAlert } = props
+
+    useEffect(() => {
+        getSummerLookOutfits()
+            .then(res => {
+                setOutfits(res.data.outfits)
+            })
+            .then(() => {
+                msgAlert({
+                    heading: 'Outfits have been retrieved!',
+                    message: indexOutfitsSuccess,
+                    variant: 'success',
+                })
+            })
+            .catch(() => {
+                msgAlert({
+                    heading: 'Failed to retrieve Outfits!',
+                    message: indexOutfitsFailure,
+                    variant: 'danger',
+                })
+            })
+    }, [])
+
+    if (!outfits) {
+        return <p>Loading ...</p>
+    } else if (outfits.length === 0) {
+        return <p>No Outfits yet, go add some</p>
+    }
+
+    let outfitCards
+
+    if (outfits.length > 0) {
+        outfitCards = outfits.map(outfit => (
+
+            < Card key={outfit._id} style={{ width: '30%' }} className="m-2" >
+                <Card.Body className="card-body d-flex flex-column justify-content-end">
+                    <Card.Title> <a href={`/outfits/user/${outfit?.owner?._id}`}>{outfit?.owner?.email}</a></Card.Title>
+                    <p><img class="outfit-image" src={outfit.img}></img></p>
+
+                    <Card.Text className="card-text">
+                        <p>Date: {outfit.date}</p>
+                        <p>Description: {outfit.description}</p>
+                        <p>Rating: {outfit.rating}</p>
+                        {outfit.tags.map(tag => (
+                            <p>Tags: {tag.category}</p>
+                        ))}
+                        <Link to={`/outfits/${outfit._id}`}>
+                            <Button variant="secondary">View</Button>
+                        </Link>
+                    </Card.Text>
+                </Card.Body>
+            </Card >
+        ))
+    }
+
+    return (
+        <>
+            <h3>All the Outfits</h3>
+            <div style={cardContainerLayout}>
+                {outfitCards}
+            </div>
+        </>
+    )
+}
+
+export default IndexSummerLookOutfits
